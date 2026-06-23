5b3cfe5e825bc794a40917727da057cbe8d91a70
Granth Bangard <155968895+GranBan@users.noreply.github.com>
2026-06-24 03:15:10 +0530

    Add 2087. Confirmation Rate [mysql]

---
+++ solutions/2087-confirmation-rate.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+select s.user_id, ifnull(round(avg(action = 'confirmed'), 2), 0) as confirmation_rate from Signups s left join Confirmations c on c.user_id = s.user_id group by s.user_id
